9db2a20fa7f1715fe43dfef75c6dae1a01d0336d
jddanzell <31313869+jddanzell@users.noreply.github.com>
2022-08-27 09:40:20 -0500

    Updated AdventD2.java
    Added all 3 directional functions
    Added case statement skeleton, all directions function as necessary
    Updated formatting

--- adventD2.java
+++ adventD2.java
@@ -7,45 +7,45 @@ public class adventD2 {
     public static void main(String args[]) {
         int horizontal = 0;
         int depth = 0;
-   
+        int switchCounter = 0;;
 
-        System.out.println(horizontal);
-
-        horizontal = forward(15, horizontal);
-        
-        System.out.println(horizontal);
-
-        horizontal = forward(15, horizontal);
-        
-        System.out.println(horizontal);
-
-        horizontal = forward(15, horizontal);
-        
-        System.out.println(horizontal);
-        
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the direction of movement " + "\n" +
+                            "\"forward\" for forwards movement, " + 
+                            "\"up\" for upwards movement, " +
+                            "\"down\" for downwards movement");
+
         
+        String userInputDirection = scan.nextLine();
        
-        switch (depth) {
-            case 1: 
+        switch (userInputDirection) {
+            case "forward":
+            System.out.println("Enter the number of units for forward movement ");
+            int userInputHorizontal = scan.nextInt();
+            System.out.println("The sub's horizontal position is " + forward(userInputHorizontal, horizontal) + "\n" +
+                               "The sub's vertical depth is " + depth);
+            break;
 
-            case 2:
+            case "up":
+            System.out.println("Enter the number of units for upwards movement ");
+            int userInputUpwards = scan.nextInt();
+            System.out.println("The sub's horizontal position is " + horizontal + "\n" +
+                               "The sub's vertical depth is " + up(userInputUpwards, depth));
+            break;
 
-            default:
+            case "down":
+            System.out.println("Enter the number of units for downwards movement ");
+            int userInputDownwards = scan.nextInt();
+            System.out.println("The sub's horizontal position is " + horizontal + "\n" +
+                               "The sub's vertical depth is " + down(userInputDownwards, depth));
+            break;
+
+            default: 
+            System.out.println("Invalid input! ");
+            break;
             
         }
-
-        switch (horizontal) {
-            case 1: 
-
-            case 2:
-
-            default:
-            
-        }
-
-        
-
 
     }
 
@@ -57,21 +57,21 @@ public class adventD2 {
 
     }
 
-    public static int up(int input) {
-        input = 0;
-        
 
-        return input;
 
-    }
-
-    public static int down(int input) {
-        input = 0;
-    
-
-    return input;
+    public static int up(int input, int depth) {
+           int newDepth = 0; 
+           newDepth = input - depth;
+           return newDepth;
 
     }
+
+    public static int down(int input, int depth) {
+        int newDepth = 0; 
+        newDepth = input + depth;
+        return newDepth;
+
+ }
  
     
 
