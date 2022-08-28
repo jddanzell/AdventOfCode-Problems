8dcb5cdaae5791782a950763f786621a005d225b
jddanzell <31313869+jddanzell@users.noreply.github.com>
2022-08-28 13:29:34 -0500

    created adventD3.java
    Started work on skeleton, plan for program

---
+++ adventD3.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+
+/* 
+ * 
+ * 
+ * 00100
+ * 11110
+ * 10110
+ * 10111
+ * 10101
+ * 01111
+ * 00111
+ * 11100
+ * 10000
+ * 11001
+ * 00010
+ * 01010
+ * Read all the arrays, push each value into a unique stack, compare frequency of stack 1 0, save it as first # in new array, pop off stack, compare next, at the end
+ * save that array then create new array with each value switched
+ * 
+ * 
+*/
+
+public class adventD3 {
+    public static void main(String[] args) {                         // main method
+        ArrayList<Integer> binaries = new ArrayList<Integer>();      // creates an ArrayList to store the given Binary Values
+        binaries.add(00100);
+        binaries.add(11110);
+        binaries.add(10110);
+        binaries.add(10111);
+        binaries.add(10101);
+        binaries.add(01111);
+        binaries.add(00111);
+        binaries.add(11100);
+        binaries.add(10000);
+        binaries.add(11001);
+        binaries.add(00010);
+        binaries.add(01010);
+
+    }
+
+
+
+    public static int frequencyCounter(ArrayList<Integer> binaries) {
+                int returnValue = 0;
+
+
+
+
+        return returnValue;
+    }
+    
+
+
+}
